b60876edc5d3d32e77b879d7914928d43676f55f
oxmc <67136658+oxmc@users.noreply.github.com>
2021-07-22 08:50:47 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,45 @@
+# Google Meet Desktop (Unofficial)
+
+**Unofficial desktop application for Google Meet built with electron.**
+
+![Screenshot of Google Meet Desktop](https://static.arjun-g.com/google-meet/google-meet-screenshot.jpg)
+
+## Features
+
+- Presenter can draw/annotate on screen when sharing the screen
+
+  ![Demo of annotation feature](https://static.arjun-g.com/google-meet/google-meet-annotation.gif)
+
+- ~~Share audio while sharing application window (Windows Only)~~ (Temporary disabled due to a audio loop issue)
+- Global shortcuts to mute/unmute microphone and switch on/off camera
+
+| Shortcut               | Use                           |
+| ---------------------- | ----------------------------- |
+| `Ctrl/⌘` + `Alt` + `A` | Toggle microphone mute/unmute |
+| `Ctrl/⌘` + `Alt` + `V` | Toggle camera on/off          |
+
+## Todo
+
+- [ ] Ability to stop screenshare without opening the main window
+- [ ] Ability to draw/annotate on presenter's screen for all participants
+- [ ] Show floating video preview when minimized similar to zoom, slack
+- [ ] Customizable shortcuts
+- [ ] Mute/umute all participants
+- [ ] Add to windows startup
+- [ ] Add ability to share audio while screensharing in mac
+- [ ] Download chat history
+
+## Installation
+
+You can [download the latest release](https://github.com/arjun-g/google-meet-desktop/releases) for your operating system or build it yourself (see below).
+
+## Building
+
+You'll need [Node.js](https://nodejs.org) installed on your computer in order to build this app.
+
+```bash
+$ git clone https://github.com/arjun-g/google-meet-desktop
+$ cd google-meet-desktop
+$ npm install
+$ npm start
+```
---
+++ src/main/window.js
@@ -0,0 +1,207 @@
+/* All window creation functions */
+const path = require("path");
+const fs = require("fs");
+const {
+  BrowserWindow,
+  BrowserView,
+  ipcMain,
+  screen,
+  app,
+} = require("electron");
+const windowStateKeeper = require("electron-window-state");
+
+const GOOGLE_MEET_URL = "https://meet.google.com/";
+
+function createMainWindow() {
+  const mainWindowState = windowStateKeeper({
+    defaultWidth: 1000,
+    defaultHeight: 800,
+    fullScreen: false,
+    maximize: true,
+  });
+
+  const mainWindow = (global.mainWindow = new BrowserWindow({
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
+    minWidth: 800,
+    minHeight: 600,
+    frame: false,
+    webPreferences: {
+      contextIsolation: true,
+      preload: path.join(__dirname, "..", "renderer", "preload.js"),
+    },
+  }));
+  mainWindowState.manage(mainWindow);
+  mainWindow.loadFile(path.join(__dirname, "..", "renderer", "index.html"));
+  // mainWindow.webContents.openDevTools();
+  mainWindow.webContents.on("did-finish-load", () => {
+    if (mainWindow.isMaximized()) {
+      mainWindow.webContents.send("window.maximized");
+    }
+  });
+
+  const googleMeetView = (global.googleMeetView = new BrowserView({
+    webPreferences: {
+      preload: path.join(
+        __dirname,
+        "..",
+        "renderer",
+        "adapters",
+        "polyfill.js"
+      ),
+    },
+  }));
+  mainWindow.setBrowserView(googleMeetView);
+  googleMeetView.webContents.loadURL(GOOGLE_MEET_URL);
+  googleMeetView.setBounds({
+    x: 0,
+    y: 40,
+    width: mainWindow.getBounds().width,
+    height: mainWindow.getBounds().height - 40,
+  });
+  googleMeetView.webContents.on("did-finish-load", () => {
+    googleMeetView.webContents.insertCSS(
+      fs
+        .readFileSync(
+          path.join(__dirname, "..", "renderer", "css", "screen.css")
+        )
+        .toString()
+    );
+  });
+  // googleMeetView.webContents.openDevTools();
+
+  mainWindow.on("resize", () => {
+    googleMeetView.setBounds({
+      x: 0,
+      y: 40,
+      width: mainWindow.getBounds().width,
+      height: mainWindow.getBounds().height - 40,
+    });
+  });
+
+  mainWindow.on("maximize", () => {
+    mainWindow.webContents.send("window.maximized");
+  });
+
+  mainWindow.on("unmaximize", () => {
+    mainWindow.webContents.send("window.restored");
+  });
+
+  ipcMain.on("window.minimize", (event) => {
+    mainWindow.minimize();
+  });
+
+  ipcMain.on("window.maximize", (event) => {
+    mainWindow.maximize();
+    event.sender.send("window.maximized");
+  });
+
+  ipcMain.on("window.restore", (event) => {
+    mainWindow.restore();
+    event.sender.send("window.restored");
+  });
+
+  ipcMain.on("window.close", () => {
+    mainWindow.close();
+  });
+
+  ipcMain.on("window.home", () => {
+    googleMeetView.webContents.loadURL(GOOGLE_MEET_URL);
+  });
+
+  let canvasWindow = createCanvasWindow();
+
+  const screenToolsWindow = createScreenToolsWindow();
+
+  // screenToolsWindow.moveAbove(canvasWindow.getMediaSourceId());
+
+  ipcMain.on("window.screenshare.show", () => {
+    mainWindow.minimize();
+    screenToolsWindow.show();
+  });
+
+  ipcMain.on("window.screenshare.hide", () => {
+    screenToolsWindow.hide();
+    screenToolsWindow.reload();
+    canvasWindow.hide();
+  });
+
+  ipcMain.on("window.canvas.show", () => {
+    canvasWindow.show();
+  });
+
+  ipcMain.on("window.canvas.hide", () => {
+    canvasWindow.hide();
+    canvasWindow.reload();
+  });
+
+  ipcMain.on("window.main.focus", () => {
+    mainWindow.restore();
+    mainWindow.focus();
+  });
+
+  ipcMain.on("screenshare.stop", () => {
+    googleMeetView.webContents.send("screenshare.stop");
+  });
+
+  mainWindow.on("closed", () => {
+    app.quit();
+  });
+}
+
+function createCanvasWindow() {
+  const primaryWorkarea = screen.getPrimaryDisplay().bounds;
+  const canvasWindow = new BrowserWindow({
+    x: primaryWorkarea.x,
+    y: primaryWorkarea.y,
+    width: primaryWorkarea.width,
+    height: primaryWorkarea.height,
+    transparent: true,
+    frame: false,
+    webPreferences: {
+      contextIsolation: true,
+      preload: path.join(__dirname, "..", "renderer", "preload.js"),
+    },
+    focusable: false,
+    show: false,
+    resizable: false,
+    skipTaskbar: true,
+  });
+  canvasWindow.webContents.loadFile(
+    path.join(__dirname, "..", "renderer", "canvas.html")
+  );
+  canvasWindow.setAlwaysOnTop(true, "pop-up-menu");
+  return canvasWindow;
+}
+
+function createScreenToolsWindow() {
+  const primaryWorkarea = screen.getPrimaryDisplay().bounds;
+  const screenToolsWindow = new BrowserWindow({
+    x: 100,
+    y: primaryWorkarea.height - 200,
+    height: 60,
+    width: 300,
+    frame: false,
+    resizable: false,
+    show: false,
+    skipTaskbar: true,
+    focusable: false,
+    transparent: true,
+    webPreferences: {
+      contextIsolation: true,
+      preload: path.join(__dirname, "..", "renderer", "preload.js"),
+    },
+  });
+
+  screenToolsWindow.setContentProtection(process.platform === "darwin");
+
+  screenToolsWindow.webContents.loadFile(
+    path.join(__dirname, "..", "renderer", "toolbar.html")
+  );
+  screenToolsWindow.setAlwaysOnTop(true, "screen-saver");
+  return screenToolsWindow;
+}
+
+module.exports = { createMainWindow };
